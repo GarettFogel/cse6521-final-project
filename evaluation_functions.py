7ed80823d9ec5d7db5554c8eadc690387d21fc85
SquiddyStardust <thecanvashat@gmail.com>
2021-12-01 12:53:31 -0500

    fixed eval functions

--- evaluation_functions.py
+++ evaluation_functions.py
@@ -3,6 +3,7 @@ import sklearn#.metrics.confusion_matrix
 import sklearn.metrics
 import numpy as np
 import random
+NUM_CLASSES = 48
 def convert_onehots_to_num(one_hots):
 	pitches = []
 	for i in one_hots:
@@ -13,20 +14,21 @@ def convert_onehots_to_num(one_hots):
 	return pitches
 
 def convertVarToHz(i):
-	midi =  (i/721)*(83-38) + 38
+	midi =  (i/(NUM_CLASSES-1))*(83-36) + 36
 	hz = 2**((midi-69)/12)*440
-	if i == 722:
+	if i == NUM_CLASSES-1:
 		return 0;
 	else:
 		return hz
 
 def convertArrayToCents(arr):
-	hzVals = np.array([convertVarToHz(xi) for xi in arr])
+	hzVals = np.array([convertVarToHz(xi) for xi in arr]).astype(float)
 	#print(arr[2])
 	hzVals = hzVals.reshape(-1)
+	#print(hzVals)
 	return mir_eval.melody.hz2cents(hzVals)
 def if722(i):
-	if i == 722:
+	if i == NUM_CLASSES-1:
 		return 0
 	else:
 		return 1
@@ -37,43 +39,21 @@ def getVoicedArray(arr):
 
 
 def rca(y_pred, y_test):
-	y_p = convert_onehots_to_num(y_pred)
-	y_test = convert_onehots_to_num(y_test)
 	#print(y_p)
 	y_pred_v = getVoicedArray(y_p)
 	y_test_v = getVoicedArray(y_test)
 	y_pred_c = convertArrayToCents(y_p)
 	y_test_c = convertArrayToCents(y_test)
 	return mir_eval.melody.raw_chroma_accuracy(y_test_v, y_test_c, y_pred_v, y_pred_c)
-
-
-def rca(y_pred, y_test):
-	y_pred = convert_onehots_to_num(y_pred)
-	y_test = convert_onehots_to_num(y_test)
-	y_pred_v = getVoicedArray(y_pred)
-	y_test_v = getVoicedArray(y_test)
-	y_pred_c = convertArrayToCents(y_pred)
-	y_test_c = convertArrayToCents(y_test)
-	return mir_eval.melody.raw_chroma_accuracy(y_test_v, y_test_c, y_pred_v, y_pred_c)
-def rca(y_pred, y_test):
-	y_pred = convert_onehots_to_num(y_pred)
-	y_test = convert_onehots_to_num(y_test)
-	y_pred_v = getVoicedArray(y_pred)
-	y_test_v = getVoicedArray(y_test)
-	y_pred_c = convertArrayToCents(y_pred)
-	y_test_c = convertArrayToCents(y_test)
-	return mir_eval.melody.raw_chroma_accuracy(y_test_v, y_test_c, y_pred_v, y_pred_c)
 def oa(y_pred, y_test):
-	y_pred = convert_onehots_to_num(y_pred)
-	y_test = convert_onehots_to_num(y_test)
+
 	y_pred_v = getVoicedArray(y_pred)
 	y_test_v = getVoicedArray(y_test)
 	y_pred_c = convertArrayToCents(y_pred)
 	y_test_c = convertArrayToCents(y_test)
 	return mir_eval.melody.overall_accuracy(y_test_v, y_test_c, y_pred_v, y_pred_c)
 def rpa(y_pred, y_test):
-	y_pred = convert_onehots_to_num(y_pred)
-	y_test = convert_onehots_to_num(y_test)
+
 	y_pred_v = getVoicedArray(y_pred)
 	y_test_v = getVoicedArray(y_test)
 	y_pred_c = convertArrayToCents(y_pred)
@@ -83,9 +63,9 @@ def rpa(y_pred, y_test):
 def generate_random_one_hots():
 	pitches = []
 	for i in range(31):
-		n = random.randrange(722)
+		n = random.randrange(NUM_CLASSES)
 		one_hot = []
-		for j in range(722):
+		for j in range(NUM_CLASSES):
 			if j == n:
 				one_hot.append(1)
 				#print(j)
@@ -95,16 +75,34 @@ def generate_random_one_hots():
 		pitches.append(one_hot)
 	return pitches
 def isVoiced(arr):
-	if arr[721] == 1:
+	if arr[NUM_CLASSES-1] == 1:
 		return 1
 	else: 
 		return 0
 def VoiceConfusionMatrix(y_pred, y_test):
-	y_pred_v = np.array([isVoiced(xi) for xi in y_pred])
-	y_test_v = np.array([isVoiced(xi) for xi in y_test])
+	y_pred_v = np.array([if722(xi) for xi in y_pred])
+	y_test_v = np.array([if722(xi) for xi in y_test])
 	return sklearn.metrics.confusion_matrix(y_test_v, y_pred_v, labels = [0, 1])
 def PitchConfusionMatrix(y_pred, y_test):
-	y_pred = convert_onehots_to_num(y_pred)
-	y_test = convert_onehots_to_num(y_test)
 	return sklearn.metrics.confusion_matrix(y_test, y_pred, labels = range(722))
 
+'''
+
+x = generate_random_one_hots()
+y = x;
+x = convert_onehots_to_num(x)
+y = convert_onehots_to_num(y)
+print(rca(x,y))
+print(rpa(x,y))
+print(oa(x,y))
+print(VoiceConfusionMatrix(x, y))
+print(PitchConfusionMatrix(x, y))
+x = generate_random_one_hots()
+y = generate_random_one_hots();
+x = convert_onehots_to_num(x)
+y = convert_onehots_to_num(y)
+print(rca(x,y))
+print(rpa(x,y))
+print(oa(x,y))
+print(VoiceConfusionMatrix(x, y))
+print(PitchConfusionMatrix(x, y))'''
